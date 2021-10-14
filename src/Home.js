e1614cb14e49a4b222bee2618b21987399617589
munroe1786 <era3munroe@gmail.com>
2021-10-14 15:30:32 -0500

    add home component

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Colors from './Colors';
+
+const Home=()=>{
+    return(
+        <>
+            <h1>This is home component</h1>
+            <Colors />
+        </>
+    )
+}
+
+export default Home;
