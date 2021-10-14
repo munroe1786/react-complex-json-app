dd4c55edd4c27b7b623a5a8fca3ce68faca76000
munroe1786 <era3munroe@gmail.com>
2021-10-14 15:52:48 -0500

    add useEffect and useState hooks and axios api call to Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Colors from './Colors';
+import axios from 'axios';
 
 const Home=()=>{
+    useEffect(()=> {
+        axios.get('https://regres.in/api/unknown')
+            .then(res=>{
+                console.log(res)
+            })
+            .catch(err=>{
+                console.log(err);
+            })
+    },[])
     return(
         <>
-            <h1>This is home component</h1>
+            <h1>{Data.Company}</h1>
+            <p>{Data.Description}</p>
             <Colors />
         </>
     )
